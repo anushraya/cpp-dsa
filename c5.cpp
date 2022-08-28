1d694c08d8cd4176b9738fdf89c5077911cf77a9
Anushraya Sharma <85569386+anushraya@users.noreply.github.com>
2022-08-28 21:08:19 +0530

    Create c5.cpp

---
+++ c5.cpp
@@ -0,0 +1,16 @@
+#include <iostream.h>
+using namespace std;
+int main()
+{
+    int a;
+    a=12;
+    cout<<"size of int"<<sizeof(a)<<endl;
+    floatb;
+    cout<<"size of float"<<sizeof(b)<<endl;
+    char c;
+    cout<<"size of char"<<sizeof(c)<<endl;
+    bool d;
+    cout<<"size of bool"<<sizeof(d)<<endl;
+    return 0;
+
+}
