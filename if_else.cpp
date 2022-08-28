9bd202cae7a20bbf738a4b4940e34022db161a4b
Anushraya Sharma <85569386+anushraya@users.noreply.github.com>
2022-08-28 21:09:25 +0530

    Create if_else.cpp

---
+++ if_else.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int savings;
+    cin>>savings;
+    if(savings>500)
+    {
+         cout<<"ab\n";
+    }
+    else if(savings>400&&savings<500)
+    {
+        cout<<"abc\n";
+    }
+    else{
+        cout<<"a\n";
+    }
+    return 0;
+
+}
