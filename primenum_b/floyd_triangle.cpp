ffc4827e74470a3a94e9f1f88bf676d87b07c412
Anushraya Sharma <85569386+anushraya@users.noreply.github.com>
2022-08-31 23:27:07 +0530

    Create floyd_triangle.cpp

---
+++ primenum_b/floyd_triangle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j;
+    cout<<"enter rows"<<endl;
+    cin>>n;
+    int count=1;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            cout<<count<<" ";
+            count++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
